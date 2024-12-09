0f0e1a8ffb9f9e60cf94c4b0614eeba92644338d
JackHanke <jack.hanke98@gmail.com>
2024-12-08 23:35:54 -0600

    random

--- models/vae/vae.py
+++ models/vae/vae.py
@@ -1,15 +1,25 @@
 from ..ann.ann import ArtificialNeuralNetwork
 import numpy as np
 
-epsilon = np.randon.normal(loc=0, scale=1, size=(1))
 
-class VariationalAutoEncoder(ArtificialNeuralNetwork):
+
+class VariationalAutoEncoder:
     def __init__(self, **kwargs):
         super(AutoEncoder, self).__init__(**kwargs)
 
-    def encoder_inference(self, activation):
-        pass
+        self.encodernet = ArtificialNeuralNetwork()
+        self.decodernet = ArtificialNeuralNetwork()
 
-    def decoder_inference(self, activation):
-        pass
+        self.loss = 0
+
+    def encoder_inference(self, activation):
+        params_vec = self.encoder_inference.inference(activation)
+        mu = params_vec[:]
+        logsig = params_vec[:]
+        epsilon = np.randon.normal(loc=0, scale=1, size=(1))
+
+        sample = g(epsilon)
+
+        inf = self.encoder_inference.inference(sample)
+
 
